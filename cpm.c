0cc5bd214769950bfa53e3e6aeb34ca8855604f8
CanDD <canddk44bk@gmail.com>
2015-05-14 21:33:28 +0700

    Add source code and document for the workshopD

---
+++ cpm.c
@@ -0,0 +1,28 @@
+/* cpm.c
+   Useful functions for Workshop E 
+*/
+#include"cpm.h"
+unsigned char bitmap[12];
+struct CPMdirent directory[64];
+
+int toggle_bit(int block)
+{
+
+	int elem=block/8;
+	int pos=block%8;
+	int mask=1<<pos;
+
+	bitmap[elem]^=mask;
+
+	return bitmap[elem]&mask;
+}
+
+
+int block_status(int block)
+{
+	int elem=block/8;
+	int pos=block%8;
+	int mask=1<<pos;
+
+	return bitmap[elem]&mask;
+}
---
+++ cpm.h
@@ -0,0 +1,48 @@
+#ifndef CPM_H
+#define CMP_H
+/* Prevent multiple inclusion */
+
+
+/* cpm.h
+   Various definitions for Workshop E
+*/
+
+/* The bitmap */
+extern unsigned char bitmap[12];
+/* 360Kb disk with 4Kb blocks -> 11.25 bytes for bitmap 
+   so round up to 12 */
+
+/* The directory entry */
+struct CPMdirent
+{
+  signed char usercode;
+         char filename[9];
+         char filetype[4];
+         char extent;
+         char blockcount;
+         char blocks[16];
+};
+/* Modelled on the description in [1].
+   
+   The two unused bytes have been added to the
+   end of the two name parameters to allow them
+   to be C strings
+*/
+
+/* The Directory */
+extern struct CPMdirent directory[128];
+
+int toggle_bit(int block);
+/* Toggles the value of the bit block, in
+   the external array bitmap.
+   returns the current value of the bit
+   Does NOT validate block!!!
+*/
+int block_status(int block);
+/* Returns if block block is allocated
+   returns 0 if bitmap bit is 0, not 
+   0 if bitmap bit is 1 
+   Does NOT validate block!!!
+*/
+#endif
+
