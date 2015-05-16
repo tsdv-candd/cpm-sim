e4a7df7715891180f333199be4b86ec1f1ffebe0
CanDD <canddk44bk@gmail.com>
2015-05-15 22:29:58 -0700

    refine menu source code

--- cpm.c
+++ cpm.c
@@ -15,11 +15,11 @@ int menu();
 
 int main ()
 {
-	int select = -1;
-	printf("Hello cpmsim!\n");
-	select = menu();
-	fprintf(stderr, "Select #%d\n", select);
-	return 0;
+    int select = -1;
+    printf("Hello cpmsim!\n");
+    select = menu();
+    fprintf(stderr, "Select #%d\n", select);
+    return 0;
 }
 
 int toggle_bit(int block)
@@ -47,25 +47,25 @@ int block_status(int block)
 /*
  * Display main menu of the program
  */
- int menu()
- {
-	int input = -1;
-	do {
-		printf("\n----------------------------------\n");
-		printf("\t\tMenu\n");
-		printf("----------------------------------\n");
-		printf("0.Exit\n");
-		printf("1.Initialise Disk\n");
-		printf("2.List Files in the Directory\n");
-		printf("3.Display the Free Bitmap\n");
-		printf("4.Open/Create File\n");
-		printf("5.Read File\n");
-		printf("6.Write File\n");
-		printf("7.Delete File\n");
-		printf("----------------------------------\n");
-		printf("Select number> ");
-		scanf("%d", &input);
-		while ( getchar() != '\n' );
-	} while ((input < 0) || (input > 7));
-	return input;
- }
+int menu()
+{
+    int input = -1;
+    do {
+        printf("\n----------------------------------\n");
+        printf("\t\tMenu\n");
+        printf("----------------------------------\n");
+        printf("0.Exit\n");
+        printf("1.Initialise Disk\n");
+        printf("2.List Files in the Directory\n");
+        printf("3.Display the Free Bitmap\n");
+        printf("4.Open/Create File\n");
+        printf("5.Read File\n");
+        printf("6.Write File\n");
+        printf("7.Delete File\n");
+        printf("----------------------------------\n");
+        printf("Select number> ");
+        scanf("%d", &input);
+        while ( getchar() != '\n' );
+    } while ((input < 0) || (input > 7));
+    return input;
+}
